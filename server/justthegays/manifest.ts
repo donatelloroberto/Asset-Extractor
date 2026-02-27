a0d2fb7395fdc2dfca631e03ea33107b31f54d32
donatelloroberto <donatelloroberto990@gmail.com>
2026-02-27 04:14:58 +0300

    Update catalog names in JUSTTHEGAYS_CATALOG_MAP

--- server/justthegays/manifest.ts
+++ server/justthegays/manifest.ts
@@ -1,19 +1,19 @@
 import type { StremioManifest } from "../stremio/manifest";
 
 export const JUSTTHEGAYS_CATALOG_MAP: Record<string, { path: string; name: string }> = {
-  "justthegays-latest": { path: "/", name: "Latest" },
-  "justthegays-anal": { path: "/categories/anal-022613/", name: "Fucking" },
-  "justthegays-black": { path: "/categories/black-022613/", name: "Black" },
-  "justthegays-dp": { path: "/categories/double-penetration-022613/", name: "Double Penetration" },
-  "justthegays-feet": { path: "/categories/feet-022613/", name: "Feet" },
-  "justthegays-fisting": { path: "/categories/fisting-022613/", name: "Fisting" },
-  "justthegays-group": { path: "/categories/group-022613/", name: "Group" },
-  "justthegays-jerkoff": { path: "/categories/jerk-off-022613/", name: "Jerk Off" },
-  "justthegays-latin": { path: "/categories/latin-022613/", name: "Latin" },
-  "justthegays-massage": { path: "/categories/massage-022613/", name: "Massage" },
-  "justthegays-oral": { path: "/categories/oral-022613/", name: "Oral" },
-  "justthegays-public": { path: "/categories/public-022613/", name: "Public" },
-  "justthegays-worship": { path: "/categories/worship-022613/", name: "Worship" },
+  "justthegays-latest": { path: "/", name: "JustTheG-Latest" },
+  "justthegays-anal": { path: "/categories/anal-022613/", name: "JustTheG-Fucking" },
+  "justthegays-black": { path: "/categories/black-022613/", name: "JustTheG-Black" },
+  "justthegays-dp": { path: "/categories/double-penetration-022613/", name: "JustTheG-Double Penetration" },
+  "justthegays-feet": { path: "/categories/feet-022613/", name: "JustTheG-Feet" },
+  "justthegays-fisting": { path: "/categories/fisting-022613/", name: "JustTheG-Fisting" },
+  "justthegays-group": { path: "/categories/group-022613/", name: "JustTheG-Group" },
+  "justthegays-jerkoff": { path: "/categories/jerk-off-022613/", name: "JustTheG-Jerk Off" },
+  "justthegays-latin": { path: "/categories/latin-022613/", name: "JustTheG-Latin" },
+  "justthegays-massage": { path: "/categories/massage-022613/", name: "JustTheG-Massage" },
+  "justthegays-oral": { path: "/categories/oral-022613/", name: "JustTheG-Oral" },
+  "justthegays-public": { path: "/categories/public-022613/", name: "JustTheG-Public" },
+  "justthegays-worship": { path: "/categories/worship-022613/", name: "JustTheG-Worship" }
 };
 
 export function buildJustthegaysManifest(): StremioManifest {
